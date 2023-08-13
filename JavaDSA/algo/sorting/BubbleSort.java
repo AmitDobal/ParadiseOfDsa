d302587102d2bfde51374e6de42caeffe97d79b7
amitd <2019pte5188@mnit.ac.in>
2023-08-13 17:20:40 +0530

    Addes: Bubble sort

---
+++ JavaDSA/algo/sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+package algo.sorting;
+
+public class BubbleSort {
+
+	public static int[] sort(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+
+			for (int j = 0; j < arr.length - i - 1; j++) {
+
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+
+		return arr;
+	}
+}
--- JavaDSA/algo/sorting/SortingTestMain.java
+++ JavaDSA/algo/sorting/SortingTestMain.java
@@ -1,7 +1,17 @@
 package algo.sorting;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class SortingTestMain {
 	public static void main(String[] args) {
-		System.out.println("HEllo Sorting");
+
+		int[] arr = { 3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48 };
+		System.out.println("Before: ");
+		Arrays.stream(arr).forEach(e -> System.out.print(e + " "));
+		BubbleSort.sort(arr);
+		System.out.println("\nAfter: ");
+		Arrays.stream(arr).forEach(e -> System.out.print(e + " "));
+
 	}
 }
