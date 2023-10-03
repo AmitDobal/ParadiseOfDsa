c303bb2857842c9701218d31ac42c8924d437028
Amit Dobal <2019pte5188@mnit.ac.in>
2023-10-04 00:24:38 +0530

    LinkedList implementation

--- JavaDSA/datastructure/linkedlist/implementation/MyLinkedList.java
+++ JavaDSA/datastructure/linkedlist/implementation/MyLinkedList.java
@@ -20,4 +20,36 @@ public class MyLinkedList {
 		}
 	}
 
+	public void print() {
+		Node temp = root;
+		System.out.print("[");
+		while (null != temp) {
+			String delimitter = ",";
+			if (null == temp.next)
+				delimitter = "";
+			System.out.print(temp.data + delimitter);
+			temp = temp.next;
+		}
+		System.out.println("]");
+	}
+
+	public void middleValue() {
+		Node slow = root;
+		Node fast = root;
+
+		while (null != root && null != fast && null != fast.next) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		System.out.println("Middle Value is: " + slow.data);
+
+	}
+	public void insert(int data, int pos) {
+		Node curr = root;
+		if(curr == null) {
+			
+		}
+	}
+
 }
---
+++ JavaDSA/datastructure/linkedlist/problems/LinkedListMain.java
@@ -0,0 +1,18 @@
+package datastructure.linkedlist.problems;
+
+import datastructure.linkedlist.implementation.MyLinkedList;
+
+public class LinkedListMain {
+	public static void main(String[] args) {
+		MyLinkedList list = new MyLinkedList();
+		list.add(2);
+		list.add(3);
+		list.add(5);
+		list.add(6);
+//		list.add(9);
+//		list.add(19);
+		
+		list.print();
+		list.middleValue();
+	}
+}
