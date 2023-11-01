0298943321fd6215e03b664a84a466e7b94cd138
Amit Dobal <2019pte5188@mnit.ac.in>
2023-11-01 11:50:37 +0530

    Tree methods

--- JavaDSA/datastructure/trees/implementation/MyTree.java
+++ JavaDSA/datastructure/trees/implementation/MyTree.java
@@ -1,32 +1,115 @@
 package datastructure.trees.implementation;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
 	TreeNode root;
-	
-	MyTree(int data){
+
+	MyTree(int data) {
 		root = new TreeNode(data);
 	}
-	
-	//Inorder Traversal
+
+	// In order Traversal
 	public void printInorder(TreeNode node) {
-		if(node == null) return ;
+		if (node == null)
+			return;
 		printInorder(node.left);
 		System.out.print(node.data + " ");
 		printInorder(node.right);
 	}
-	//PreOrder Traversal
+
+	// PreOrder Traversal
 	public void printPreorder(TreeNode node) {
-		if(node == null) return ;
+		if (node == null)
+			return;
 		System.out.print(node.data + " ");
-		printInorder(node.left);
-		printInorder(node.right);
+		printPreorder(node.left);
+		printPreorder(node.right);
 	}
-	//PostOrder Traversal
+
+	// PostOrder Traversal
 	public void printPostorder(TreeNode node) {
-		if(node == null) return ;
-		printInorder(node.left);
-		printInorder(node.right);
+		if (node == null)
+			return;
+		printPostorder(node.left);
+		printPostorder(node.right);
 		System.out.print(node.data + " ");
+	}
+
+	// Height
+	public int height(TreeNode root) {
+		if (root == null)
+			return 0;
+
+		return Math.max(height(root.left), height(root.right)) + 1;
+	}
+
+	// Print K distance
+	public void printKDist(TreeNode root, int k) {
+		if (null == root)
+			return;
+
+		if (k == 0)
+			System.out.print(root.data + " ");
+		else {
+			printKDist(root.left, k - 1);
+			printKDist(root.right, k - 1);
+		}
+	}
+
+	// Level order traversal
+	public void printLevelOrder() {
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(this.root);
+
+		while (!q.isEmpty()) {
+			TreeNode curr = q.poll();
+			System.out.print(curr.data + " ");
+			if (curr.left != null)
+				q.add(curr.left);
+			if (curr.right != null)
+				q.add(curr.right);
+		}
+	}
+
+	// Level order traversal Line by Line
+	public void printLevelOrderByLine() {
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(this.root);
+		q.add(null);
+
+		while (q.size() > 1) {
+			TreeNode curr = q.poll();
+			if (curr == null) {
+				System.out.println();
+				q.add(null);
+				continue;
+			}
+			System.out.print(curr.data + " ");
+			if (curr.left != null)
+				q.add(curr.left);
+			if (curr.right != null)
+				q.add(curr.right);
+		}
+	}
+
+	// Level order traversal Line by Line
+	public void printLevelOrderByLine2() {
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(this.root);
+		while (!q.isEmpty()) {
+			int count = q.size();
+			for (int i = 0; i < count; i++) {
+				TreeNode curr = q.poll();
+				System.out.print(curr.data + " ");
+				if (curr.left != null)
+					q.add(curr.left);
+				if (curr.right != null)
+					q.add(curr.right);
+			}
+			System.out.println();
+		}
 	}
 
 }
--- JavaDSA/datastructure/trees/implementation/TreeMain.java
+++ JavaDSA/datastructure/trees/implementation/TreeMain.java
@@ -3,21 +3,35 @@ package datastructure.trees.implementation;
 public class TreeMain {
 	public static void main(String[] args) {
 		MyTree tree = new MyTree(10);
-		
+
 		tree.root.left = new TreeNode(20);
-		tree.root.right = new TreeNode(30, new TreeNode(40), new TreeNode(50));
-		
+		tree.root.right = new TreeNode(30, new TreeNode(40, new TreeNode(60), new TreeNode(70)),
+				new TreeNode(50, new TreeNode(80)));
+
 		System.out.println("Inorder: ");
 		tree.printInorder(tree.root);
 		System.out.println();
-		
+
 		System.out.println("PreOrder: ");
 		tree.printPreorder(tree.root);
 		System.out.println();
-		
+
 		System.out.println("PostOrder: ");
 		tree.printPostorder(tree.root);
 		System.out.println();
+
+		System.out.println("Height of the Tree: " + tree.height(tree.root));
+
+		System.out.println("Print k distance: ");
+		tree.printKDist(tree.root, 3);
+		System.out.println();
+
+		System.out.println("Level Order traversal: ");
+		tree.printLevelOrder();
+		System.out.println();
 		
+		System.out.println("Level Order traversal Line By Line: ");
+		tree.printLevelOrderByLine2();
+		System.out.println();
 	}
 }
